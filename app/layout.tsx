2dad44d11a33bd3fc99251640bab88f61cf7ffe4
Lakshay Rathore <lakshayrathore1879@gmail.com>
2025-12-10 11:10:06 +0530

    feat: create initial app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "@/components/theme-provider"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +13,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={'bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white'}>
+
+        {/* Wrapping with ThemeProvider dark mode */}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+
+          {/* Header */}
+
+          <main className="relative min-h-screen container mx-auto pt-40 md:pt-32 " >
+            {/* Background glow effects (behind everything) */}
+            <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+              <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+              <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
+            </div>
+
+            {/* Page content (above glow) */}
+            <div className="relative z-10">{children}</div>
+          </main>
+
+          {/* Footer */}
+
+
+        </ThemeProvider>
+
       </body>
     </html>
   );
